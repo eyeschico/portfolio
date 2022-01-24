58e5ccb8cd65f44d6a69490d3eddddc810516fa7
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2022-01-24 15:09:19 +0100

    add darkmode

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "24167894c62615f0"; }
+/******/ 		__webpack_require__.h = function() { return "50643af74865b392"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/navbar.js
+++ components/navbar.js
@@ -15,7 +15,7 @@ import {
   useColorModeValue
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
-import { ST } from 'next/dist/shared/lib/utils'
+import ThemeToggleButton from './theme-toggle-button'
 
 const LinkItem = ({ href, path, children}) => {
   const active = path === href
@@ -77,6 +77,7 @@ const Navbar = props => {
         </Stack>
 
         <Box flex={1} align="right">
+          <ThemeToggleButton/>
           <Box ml={2} display={{base: 'inline-block', md:'none'}}>
             <Menu>
               <MenuButton 
