7525d5580b40e2557c6f4795e875150c60b9bd8e
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2022-02-08 17:13:17 +0100

    Add footer

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,12 +1,18 @@
 import Head from 'next/head'
 import Navbar from '../navbar.js'
 import { Box, Container} from '@chakra-ui/react'
+import Footer from '../footer'
 
 const Main = ({children, router}) => {
   return (
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="description" content="Mohammed's homepage" />
+        <meta name="author" content="Mohammed Khalef"/>
+        <meta name="author" content="eyeschico"/>
+        <meta property="og:site_name" content="Mohammed Khalef's Homepage" />
+        <meta property="og:type" content="website" />
         <title>Mohammed Khalef</title>
       </Head>
 
@@ -14,6 +20,7 @@ const Main = ({children, router}) => {
 
       <Container maxW="container.lg.md" pt={14}>
         {children}
+        <Footer/>
       </Container>
     </Box>
   )
