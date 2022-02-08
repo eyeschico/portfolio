d3896f5f34adbf3ebfc4e86bc04ba6a2e6be7028
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2022-02-08 17:04:36 +0100

    minor

--- pages/posts.js
+++ pages/posts.js
@@ -1,7 +1,6 @@
 import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { GridItem } from '../components/grid-item'
 import { PostGridItem } from '../components/grid-item'
 
 
