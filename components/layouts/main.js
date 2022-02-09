baa583079ec2da600215a2e5c7c4104eb147075d
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2022-02-09 11:38:33 +0100

    Add meta and social media information + fix font with styled jsx

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -11,6 +11,15 @@ const Main = ({children, router}) => {
         <meta name="description" content="Mohammed's homepage" />
         <meta name="author" content="Mohammed Khalef"/>
         <meta name="author" content="eyeschico"/>
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@eyeschico" />
+        <meta name="twitter:creator" content="@eyeschico" />
+        <meta name="twitter:image" content="/card.png" />
+
+        <meta property="og:image" content="/card.png" />
         <meta property="og:site_name" content="Mohammed Khalef's Homepage" />
         <meta property="og:type" content="website" />
         <title>Mohammed Khalef</title>
